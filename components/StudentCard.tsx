a7e2864cf208473b9c70fcc1c933527106bf285c
alexcsalinas <alexsali@umich.edu>
2025-08-31 22:34:37 -0400

    hover

--- components/StudentCard.tsx
+++ components/StudentCard.tsx
@@ -15,27 +15,27 @@ export function StudentCard({ student, index }: StudentCardProps) {
         animationFillMode: 'both'
       }}
     >
-      {/* Flowing background effect on hover */}
+      {/* Subtle flowing background effect on hover */}
       <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-all duration-700">
-        {/* Top-left flowing corner */}
-        <div className="absolute top-0 left-0 w-0 h-0 border-l-2 border-t-2 border-maize/40 dark:border-maize/50 group-hover:w-8 group-hover:h-8 transition-all duration-500 ease-out"></div>
+        {/* Top-left flowing corner - more subtle */}
+        <div className="absolute top-0 left-0 w-0 h-0 border-l border-t border-maize/30 dark:border-maize/40 group-hover:w-6 group-hover:h-6 transition-all duration-400 ease-out"></div>
         
-        {/* Top-right flowing corner */}
-        <div className="absolute top-0 right-0 w-0 h-0 border-r-2 border-t-2 border-maize/40 dark:border-maize/50 group-hover:w-8 group-hover:h-8 transition-all duration-500 ease-out delay-100"></div>
+        {/* Top-right flowing corner - more subtle */}
+        <div className="absolute top-0 right-0 w-0 h-0 border-r border-t border-maize/30 dark:border-maize/40 group-hover:w-6 group-hover:h-6 transition-all duration-400 ease-out delay-75"></div>
         
-        {/* Bottom-left flowing corner */}
-        <div className="absolute bottom-0 left-0 w-0 h-0 border-l-2 border-b-2 border-maize/40 dark:border-maize/50 group-hover:w-8 group-hover:h-8 transition-all duration-500 ease-out delay-200"></div>
+        {/* Bottom-left flowing corner - more subtle */}
+        <div className="absolute bottom-0 left-0 w-0 h-0 border-l border-b border-maize/30 dark:border-maize/40 group-hover:w-6 group-hover:h-6 transition-all duration-400 ease-out delay-150"></div>
         
-        {/* Bottom-right flowing corner */}
-        <div className="absolute bottom-0 right-0 w-0 h-0 border-r-2 border-b-2 border-maize/40 dark:border-maize/50 group-hover:w-8 group-hover:h-8 transition-all duration-500 ease-out delay-300"></div>
+        {/* Bottom-right flowing corner - more subtle */}
+        <div className="absolute bottom-0 right-0 w-0 h-0 border-r border-b border-maize/30 dark:border-maize/40 group-hover:w-6 group-hover:h-6 transition-all duration-400 ease-out delay-225"></div>
       </div>
       
-      {/* Flowing glow effect that radiates outward */}
+      {/* More subtle flowing glow effect */}
       <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-all duration-700">
-        <div className="absolute top-1/2 left-1/2 w-0 h-0 bg-gradient-to-r from-maize/20 to-yellow-400/20 rounded-full blur-xl group-hover:w-32 group-hover:h-32 -translate-x-1/2 -translate-y-1/2 transition-all duration-700 ease-out"></div>
+        <div className="absolute top-1/2 left-1/2 w-0 h-0 bg-gradient-to-r from-maize/10 to-yellow-400/10 rounded-full blur-lg group-hover:w-24 group-hover:h-24 -translate-x-1/2 -translate-y-1/2 transition-all duration-600 ease-out"></div>
       </div>
       
-      {/* Subtle text glow on hover */}
+      {/* Content with subtle text glow on hover */}
       <div className="relative z-10 flex flex-col h-full justify-center">
         {student.website && (
           <a
@@ -53,17 +53,17 @@ export function StudentCard({ student, index }: StudentCardProps) {
               className="opacity-0 group-hover:opacity-100 transition-all duration-300 flex-shrink-0 text-maize group-hover:scale-110 group-hover:rotate-12 relative z-10" 
             />
             
-            {/* Flowing underline effect */}
-            <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-gradient-to-r from-maize to-yellow-400 group-hover:w-full transition-all duration-500 ease-out"></div>
+            {/* More subtle flowing underline effect */}
+            <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-gradient-to-r from-maize/60 to-yellow-400/60 group-hover:w-full transition-all duration-400 ease-out"></div>
           </a>
         )}
       </div>
       
-      {/* Subtle floating particles effect */}
+      {/* More subtle floating particles effect */}
       <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-700 pointer-events-none">
-        <div className="absolute top-2 left-3 w-1 h-1 bg-maize/60 rounded-full animate-pulse"></div>
-        <div className="absolute top-4 right-4 w-0.5 h-0.5 bg-maize/40 rounded-full animate-pulse delay-300"></div>
-        <div className="absolute bottom-3 left-4 w-1.5 h-1.5 bg-maize/50 rounded-full animate-pulse delay-500"></div>
+        <div className="absolute top-2 left-3 w-0.5 h-0.5 bg-maize/50 rounded-full animate-pulse"></div>
+        <div className="absolute top-4 right-4 w-0.5 h-0.5 bg-maize/30 rounded-full animate-pulse delay-300"></div>
+        <div className="absolute bottom-3 left-4 w-1 h-1 bg-maize/40 rounded-full animate-pulse delay-500"></div>
       </div>
     </div>
   )
